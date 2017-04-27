2a837d646d7ffd20390b5eef9987538e86b1c4f6
dexter aparicio <syscools@gmail.com>
2017-04-28 02:53:25 +0800

    add setup.sh inside testdata

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 #
-mkdir -m 700 ~/.ssh 2>/dev/null
 cp dotvimrc ~/.vimrc
 cp dot_tmux.conf ~/.tmux.conf
 cp dotgitconfig ~/.gitconfig
-cp config ~/.ssh/
 # this is a test2
 # not yet, since we have not setup our pub/priv key
 #git remote set-url origin git+ssh://github.com/syscools/commandline.git
